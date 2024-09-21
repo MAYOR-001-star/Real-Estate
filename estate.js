7e4b47d4906a374e78d9e87ecdb4e4efaa1abd36
baruwa abdul-azeez mayowa <mayorkunabdulazeez01@gmail.com>
2024-09-22 00:51:21 +0100

    made use of swiper.js for an image slider

--- estate.js
+++ estate.js
@@ -1,1 +1,26 @@
-document.write("motherfucker")
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    centeredSlides: false,
+    spaceBetween: 0,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1, // 1 slide
+        },
+        // when window width is <= 768px
+        768: {
+            slidesPerView: 2, // 2 slides
+        },
+        // when window width is > 768px
+        769: {
+            slidesPerView: 3, // 3 slides
+        },
+    },
+});
